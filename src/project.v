d234e1711020b54906774e0038a33b6d32b8f49f
Tobias <tobias.devries.mail@gmail.com>
2026-09-17 19:56:16 -0400

    small syntax fisxes on project.v

--- src/project.v
+++ src/project.v
@@ -9,10 +9,10 @@ module tt_um_counter(
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
     input  wire       ena,      // always 1 when the design is powered, so you can ignore it
     input  wire       clk,      // clock
-    input  wire       rst_n,     // reset_n - low to reset
+    input  wire       rst_n     // reset_n - low to reset
 );
   reg [7:0] count_reg;
-  wire load;
+    wire load = uio_in [0];
   
     always @(posedge clk or negedge rst_n) begin
       if(!rst_n) begin
@@ -26,4 +26,7 @@ module tt_um_counter(
     end
   end
   assign uo_out = ena ? count_reg : 8'bz;
+
+    assign uio_out = 8'b0;
+    assign uio_oe  = 8'b0;
 endmodule
